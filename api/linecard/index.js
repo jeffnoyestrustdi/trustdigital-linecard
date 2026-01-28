3ecf4af6be41369e7746f2eb258f96c9e61e59e2
Jeff Noyes <you@trustdigital.net>
2026-01-28 16:42:28 -0600

    Temp: surface API error

--- api/linecard/index.js
+++ api/linecard/index.js
@@ -1,21 +1,32 @@
 const { getClient } = require("../_shared/table");
 
 module.exports = async function (context, req) {
-  const client = await getClient();
+  try {
+    const client = await getClient();
 
-  for await (const e of client.listEntities()) {
-    entities.push({
-      id: e.rowKey,
-      vendor: e.vendor,
-      category: e.category,
-      primaryOffering: e.primaryOffering,
-      secondaryOffering: e.secondaryOffering,
-      tags: e.tags ? JSON.parse(e.tags) : []
-    });
+    const entities = [];
+    for await (const e of client.listEntities()) {
+      entities.push({
+        id: e.rowKey,
+        vendor: e.vendor,
+        category: e.category,
+        primaryOffering: e.primaryOffering,
+        secondaryOffering: e.secondaryOffering,
+        tags: e.tags ? JSON.parse(e.tags) : []
+      });
+    }
+
+    context.res = {
+      status: 200,
+      headers: { "content-type": "application/json" },
+      body: entities
+    };
+  } catch (err) {
+    context.log("LINECARD ERROR:", err);
+    context.res = {
+      status: 500,
+      headers: { "content-type": "text/plain" },
+      body: err?.stack || String(err)
+    };
   }
-
-  context.res = {
-    headers: { "content-type": "application/json" },
-    body: entities
-  };
 };
